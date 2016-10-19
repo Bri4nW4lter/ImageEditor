51fe921c7624d7db4e0886af0da59c1d028936f1
Mampe <konrad.mampe@haw-hamburg.de>
2016-10-19 15:26:46 +0200

    Created open Menu

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -7,15 +7,35 @@ import javax.imageio.*;
 
 public class ImageEditorFrame extends JFrame{
 	public ImageEditorFrame(){
+		createMenuBar();
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(400, 300);
 		setVisible(true);
 	}
 	
+	private void createMenuBar(){
+	JMenuBar menuBar = new JMenuBar();
+	setJMenuBar(menuBar);
+	JMenu menuFile = new JMenu("File");
+	menuBar.add(menuFile);
+	JMenuItem menuItemOpen = new JMenuItem("Open");
+	menuFile.add(menuItemOpen);
+	menuItemOpen.addActionListener(
+		new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				onOpen();
+			}
+		}
+	);
+}
+
+private void onOpen(){
+	JOptionPane.showMessageDialog(this, "Open Selected");
+}
 	
 	public static void main(String[] args){
-		ImageEditorFrame frame = new ImageEditorFrame();
-			
+		ImageEditorFrame frame = new ImageEditorFrame();	
 		
 	}	
 }
+
