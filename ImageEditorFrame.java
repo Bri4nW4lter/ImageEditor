d471b20314b819feca5c08044e6a86b70abf6993
Walter <brian.walter@haw-hamburg.de>
2016-10-19 14:57:14 +0200

    ImageEditorFrame added

---
+++ ImageEditorFrame.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import javax.awt.event.*;
+import javax.awt.*;
+import javax.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+
+public class ImageEditorFrame extends JFrame{
+	public ImageEditorFrame(){
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(400, 300);
+		setVisible(true);
+	}
+}
